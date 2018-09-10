37c3d0bbb77c91f20073a4914465b66b68a9b5bd
Moe Zeid <m.zeid97@ku.edu>
2018-09-10 10:10:12 -0700

    Added functionality on Executive run

--- Executive.h
+++ Executive.h
@@ -1,4 +1,4 @@
-#if#ifndef EXECUTIVE_H
+#ifndef EXECUTIVE_H
 #define EXECUTIVE_H
 #include "Minesweeper.h"
 
@@ -8,10 +8,11 @@ class Executive
 	public:
 		Executive();
 		Executive(std::string row, std::string col, std::string numMines);
-		void run(int row, int col, int numMines);
+		void run();
 	private:
-
+		int m_row;
+		int m_col;
+		int m_mines;
 
 };
-#endif
 #endif
