b7b7ff7b8aed53039c85899169cc05e5bb38510b
Yizhou <wyizhou222@gmail.com>
2018-09-16 19:15:40 -0500

    update

--- main.cpp
+++ main.cpp
@@ -41,3 +41,114 @@ int main(int argc, char* argv[])
 		}			//end failbit
 
 
+		
+		while(row<2|| row>40)   //checks to see if row is in bounds
+		{
+			std::cout<<"The number of rows you provided is out of bounds. The lower bound is 2 and the upper bound is 4.\n Enter # of rows: ";
+			std::cin>>row;
+
+
+
+			while(std::cin.fail()) //failbit
+			{
+				std::cin.clear();
+				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+				std::cout<<"Please enter a number!\n Row:";
+				std::cin>>row;
+				std::cout<<endl;
+			}			//end failbit
+
+
+
+
+		}
+
+
+
+		std::cout<<"Enter the number of columns you would like: ";
+		std::cin>>col;
+
+/*--------------------------------------------------------------------columns failbit------------------------------------------------------------------*/
+		while(std::cin.fail()) //failbit
+		{
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			std::cout<<"Please enter a number!\n Col:";
+			std::cin>>col;
+			std::cout<<endl;
+		}			//end failbit
+
+
+
+		
+		while(col<2|| col>40)   //checks to see if row is in bounds
+		{
+			std::cout<<"The number of columns you provided is out of bounds. The lower bound is 2 and the upper bound is 4.\n Enter # of columns: ";
+			std::cin>>col;
+
+
+
+			while(std::cin.fail()) //failbit
+			{
+				std::cin.clear();
+				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+				std::cout<<"Please enter a number!\n Row:";
+				std::cin>>col;
+				std::cout<<endl;
+			}			//end failbit
+
+
+
+
+		}
+
+
+	maxMines=(row*col)-1;
+
+	std::cout<<"Enter the number of mines you would like: ";
+	std::cin>>mines;
+	
+/*--------------------------------------------------------------------mines failbit------------------------------------------------------------------*/
+	while(std::cin.fail()) //failbit
+	{
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		std::cout<<"Please enter a number!\n Mines:";
+		std::cin>>mines;
+		std::cout<<endl;
+	}			//end failbit
+
+
+
+		
+	while(mines<1 || mines>maxMines)   //checks to see if row is in bounds
+	{
+		std::cout<<"The number of mines you provided is out of bounds. The lower bound is 1 and the upper bound is "<<maxMines<<"\n Enter # of mines: ";
+		std::cin>>mines;
+
+
+
+		while(std::cin.fail()) //failbit
+		{
+			std::cin.clear();
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			std::cout<<"Please enter a number!\n Mines:";
+			std::cin>>mines;
+			std::cout<<endl;
+		}			//end failbit
+
+
+
+
+	}
+	
+		
+
+	Executive exec(row,col,mines);
+	exec.run();
+
+	}
+
+	
+return(0);
+}
